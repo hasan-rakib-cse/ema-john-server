49c2fdf850141dfae382dfb36963a814d76368f2
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-11-29 01:04:55 +0600

    mongodb er uri te database er name insert korsi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kiav5mh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kiav5mh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
 
